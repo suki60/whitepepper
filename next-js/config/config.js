964d2bf13e3eb3c845cb75e5712444614b3563b3
francesc <fran.altes@gmail.com>
2020-10-28 01:05:31 +0100

    feat: add clientApiUrl

--- next-js/config/config.js
+++ next-js/config/config.js
@@ -1,4 +1,5 @@
 export default {
   serverApiUrl: "http://sinatra:9292",
-  clientApiUrl: "http://localhost:9292",
+  clientApiUrl: "http://15.236.197.180:9292",
+  localHostUrl: "http://localhost:9292"
 }
