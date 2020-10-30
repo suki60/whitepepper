e22961b444d12ccb945feb9414e6d343c052e02d
francesc <fran.altes@gmail.com>
2020-10-30 09:58:26 +0000

    feat: update headers

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -35,7 +35,7 @@ class WhitePepper < Sinatra::Base
 
   before do
     headers(
-      { 'Access-Control-Allow-Origin' => 'http://localhost:3000',
+      { 'Access-Control-Allow-Origin' => '*',
         'Access-Control-Allow-Credentials' => 'true',
         'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers' => 'Content-Type,
