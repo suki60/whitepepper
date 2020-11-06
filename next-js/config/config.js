fb1486882da95801c330b794cabd6c099dd8c9a3
francesc <fran.altes@gmail.com>
2020-11-06 12:15:34 +0000

    feat: update wp url config

--- next-js/config/config.js
+++ next-js/config/config.js
@@ -1,5 +1,5 @@
 export default {
   serverApiUrl: "http://sinatra:9292",
-  clientApiUrl: "http://3.91.218.39:9292",
+  clientApiUrl: "http://www.white-pepper:9292",
   // clientApiUrl: "http://localhost:9292",
 }
