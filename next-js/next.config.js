b52daa47964cdcb84c35128a716d1723ec0fa427
suki60 <fran.altes@gmail.com>
2023-08-25 17:05:35 +0200

    javascript -> typescript

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -1,10 +1,9 @@
-/* eslint no-param-reassign: ["error", { "props": false }] */
-const path = require('path')
-
 module.exports = {
-  webpack: (config) => {
-    config.resolve.alias.components = path.join(__dirname, 'components')
-    config.resolve.alias.config = path.join(__dirname, 'config')
-    return config
-  }
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
