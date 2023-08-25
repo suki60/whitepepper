4b3a6e9e7aa671da1069c3eb45e9afd01be09fab
suki60 <fran.altes@gmail.com>
2023-08-25 13:02:03 +0200

    eslint fix

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 module.exports = {
   webpack: (config) => {
     config.resolve.alias.components = path.join(__dirname, 'components')
-    config.resolve.alias.config = path.join(__dirname, "config")
+    config.resolve.alias.config = path.join(__dirname, 'config')
     return config
   }
 }
